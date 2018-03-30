049247be9d1b0921b113bae3ac06b438214e794e
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2018-03-30 18:01:43 -0400

    Remove unused build.clj
    New cljs.main allows all of this to be passed as command-line arguments, which
    is done in the build script.

--- build.clj
+++
@@ -1,6 +0,0 @@
-(require 'cljs.build.api)
-
-(cljs.build.api/build "src" {:main 'com.rentpath.jj.cli
-                             :output-to "out/jj"
-                             :target :nodejs
-                             :optimizations :advanced})
